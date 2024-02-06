06ef369ca92f8ede8d3cdd48e5b792476e27dbc5
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-02-05 18:50:20 -0700

    refactor(event): ping.js to ts

--- src/bot/commands/misc/ping.ts
+++ src/bot/commands/misc/ping.ts
@@ -4,7 +4,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    Interaction,
+} = require("discord.js");
 const config = require("../../config");
 const logger = require("../../../utils/logger");
 
@@ -12,7 +16,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription(`Pings Liberation!`),
-    async execute(interaction) {
+    async execute(interaction: typeof Interaction) {
         // Grab client latency
         const latency = Math.abs(Date.now() - interaction.createdAt);
 
