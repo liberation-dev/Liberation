04898f96a50bfcbe5458ba42baf7b3c4085eb8ad
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-02-05 18:38:21 -0700

    feat: add license notice to files

--- src/bot/commands/misc/ping.js
+++ src/bot/commands/misc/ping.js
@@ -1,3 +1,9 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const config = require("../../config");
 const logger = require("../../../utils/logger");
