a56929b7f3cd23e0541b1f777fb67a0cfdc14a45
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-02-05 18:49:33 -0700

    refactor(event): ready_once.js to ts

--- src/bot/events/ready_once.ts
+++ src/bot/events/ready_once.ts
@@ -4,14 +4,14 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-const { Events, ActivityType } = require("discord.js");
+const { Events, ActivityType, Client } = require("discord.js");
 const config = require("../config");
 const logger = require("../../utils/logger");
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
-    async execute(client) {
+    async execute(client: typeof Client) {
         logger.info(`Logged in as ${client.user.tag}`);
 
         client.user.setPresence({
