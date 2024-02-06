72b75667ee5e10b5ea5b9d73bef1a815470283b5
Jaden <100730821+JadenLabs@users.noreply.github.com>
2024-02-05 18:41:06 -0700

    refactor(event): register.js to ts

--- src/bot/scripts/register.ts
+++ src/bot/scripts/register.ts
@@ -17,7 +17,7 @@ for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
     const commandFiles = fs
         .readdirSync(commandsPath)
-        .filter((file) => file.endsWith(".js"));
+        .filter((file: string) => file.endsWith(".js"));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
